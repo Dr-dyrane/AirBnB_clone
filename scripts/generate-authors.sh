fed01498e2b251cf3ef74b22e65e3de68844f0ac
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-07-08 11:28:51 +0100

    Authors scripts

---
+++ scripts/generate-authors.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+###############################################################################
+# Script name: generate-authors.sh
+# Description: Script to generate the AUTHORS file for the project.
+#              This script retrieves the author names and emails from the Git commit history
+#              and generates the "AUTHORS" file in the root directory of the project.
+#
+# Usage: ./scripts/generate-authors.sh
+###############################################################################
+
+set -e
+
+# Change to the root directory of the project
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+# Header for the AUTHORS file
+{
+    cat <<-'EOH'
+        # This file lists all individuals having contributed content to the repository.
+        # For how it is generated, see `scripts/generate-authors.sh`.
+    EOH
+    echo
+} > AUTHORS
+
+# Retrieve author names and emails from Git commit history, sort, and remove duplicates
+# git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf >> AUTHORS
+
+# Notify the user that the AUTHORS file has been generated
+# echo "The AUTHORS file has been generated successfully."
