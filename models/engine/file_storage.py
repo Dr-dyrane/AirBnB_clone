94f3b97b532fbc799b23bdcec40e605b16768659
Dr Dyrane Alexander <Ogranya.Alex@gmail.com>
2023-07-17 00:17:36 +0100

    file storage fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -60,7 +60,7 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """
         Retrieves all objects or objects of a specific class.
 
@@ -70,28 +70,35 @@ class FileStorage:
         Returns:
             dict: A dictionary containing the retrieved objects.
         """
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            filtered_objects = {}
+            for key, value in self.__objects.items():
+                if isinstance(value, cls):
+                    filtered_objects[key] = value
+            return filtered_objects
 
     def new(self, obj):
         """Sets new obj in __objects dictionary."""
         key = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to JSON file."""
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in self.__objects.items()}
             json.dump(d, f)
 
     def reload(self):
         """Deserializes JSON file into __objects."""
-        if not os.path.isfile(FileStorage.__file_path):
+        if not os.path.isfile(self.__file_path):
             return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+        with open(self.__file_path, "r", encoding="utf-8") as f:
             obj_dict = json.load(f)
             obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
-            FileStorage.__objects = obj_dict
+            self.__objects = obj_dict
 
     def classes(self):
         """Returns a dictionary of valid classes and their references."""
@@ -103,19 +110,17 @@ class FileStorage:
         from models.place import Place
         from models.review import Review
 
-        classes = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Place": Place,
-            "Review": Review
-        }
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
         return classes
 
-    def attributes(self):
-        """Returns the valid attributes and their types for each class."""
+    def attributes(self, cls_name):
+        """Returns the valid attributes and their types for a given class name."""
         attributes = {
             "BaseModel": {
                 "id": str,
@@ -157,4 +162,11 @@ class FileStorage:
                 "text": str
             }
         }
-        return attributes
+        return attributes[cls_name]
+
+
+if __name__ == "__main__":
+    storage = FileStorage()
+    storage.reload()
+    objects = storage.all()
+    print(objects)
